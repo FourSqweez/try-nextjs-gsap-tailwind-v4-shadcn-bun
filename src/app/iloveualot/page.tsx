0664585b53ed59880c150fec6e866f2399523045
Foursqweez <jeerasak.code@gmail.com>
2025-03-10 20:00:35 +0700

    Create countdown gift for my gf

---
+++ src/app/iloveualot/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import confetti from "canvas-confetti";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [count, setCount] = useState(3);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (!isActive || count === 0) return; // Only start the countdown if active and count is not 0
+
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+
+    // Clear the interval when the countdown is finished or the component unmounts
+    return () => clearInterval(timer);
+  }, [isActive, count]);
+
+  useEffect(() => {
+    if (count === 0) {
+      handleConfetti();
+    }
+  }, [count]);
+
+  const startCountdown = () => {
+    setIsActive(true); // Start the countdown when the button is clicked
+  };
+
+  const handleConfetti = () => {
+    confetti({});
+  };
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-white">
+      {count !== 0 ? (
+        <h1
+          className={`text-[200px] font-bold transition-all duration-500 ease-in-out ${
+            count === 0 ? "scale-110 text-red-500" : "scale-100 text-red-500"
+          }`}
+        >
+          {count}
+        </h1>
+      ) : (
+        <Image
+          src="/d8_iloveualot.png"
+          alt="iloveualot"
+          className={cn(
+            `mx-10 hidden h-[450px] w-[340px] border-2 border-red-500 object-fill px-10 sm:h-[600px] sm:w-[400px]`,
+            {
+              block: count === 0,
+            },
+          )}
+          width={1000}
+          height={1000}
+        />
+      )}
+
+      {count !== 0 && (
+        <Button
+          onClick={startCountdown}
+          className="cursor-pointer border-2 border-red-400 text-red-400 outline-red-400 hover:text-red-400"
+          variant="outline"
+        >
+          Click me
+        </Button>
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className="bg-kitty-dark flex h-screen w-full">
+    <div className="bg-kitty-dark h-screen w-full">
       <h1 className="text-kitty-green-pale text-5xl">Hello</h1>
+      <div className="flex gap-4 text-white">
+        {Array.from({ length: 5 }, (_, i) => (
+          <div key={i}>Item {i + 1}</div>
+        ))}
+      </div>
     </div>
   );
 }
