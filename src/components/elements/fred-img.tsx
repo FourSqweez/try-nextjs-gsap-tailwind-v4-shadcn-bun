d77a1558b84ecc54fe913973f2c6514864ed5a4b
Foursqweez <jeerasak.code@gmail.com>
2025-03-11 18:57:29 +0700

    Stagger with share amount of 1 second

--- src/components/elements/fred-img.tsx
+++ src/components/elements/fred-img.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { cn } from "@/lib/utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { useRef } from "react";
@@ -7,25 +6,43 @@ import { useRef } from "react";
 gsap.registerPlugin(useGSAP);
 
 export default function FredImg() {
-  const fredRef = useRef<null>(null);
-
-  useGSAP(() => {
-    gsap.to(".green", { duration: 3, x: 600, ease: "bounce" });
-    gsap.to(".pink", { duration: 3, x: 600, ease: "back(6)" });
-  }, {});
+  const fredsContainer = useRef<null>(null);
+  useGSAP(
+    () => {
+      gsap.to("img", {
+        y: -100,
+        stagger: {
+          amount: 1,
+        },
+      });
+    },
+    { scope: fredsContainer },
+  );
 
   return (
-    <div ref={fredRef} className="mt-10 h-fit">
-      <img
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred.svg"
-        alt=""
-        className={cn("green h-auto w-fit object-fill")}
-      />
-      <img
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred-pink.svg"
-        alt=""
-        className={cn("pink h-auto w-fit object-fill")}
-      />
+    <div
+      id="demo"
+      className="relative flex h-screen w-full justify-center bg-[url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/space-background.png')] bg-cover bg-center"
+    >
+      <div ref={fredsContainer} className="absolute bottom-30 flex gap-x-2">
+        <FredImage />
+        <FredImage color="purple" />
+        <FredImage color="blue" />
+        <FredImage color="pink" />
+        <FredImage color="orange" />
+        <FredImage color="red" />
+      </div>
     </div>
   );
 }
+
+type FredColor = "purple" | "blue" | "pink" | "orange" | "red";
+interface FredImageProps {
+  color?: FredColor;
+}
+
+const FredImage: React.FC<FredImageProps> = ({ color }) => {
+  const imageUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred${color ? `-${color}` : ""}.svg`;
+
+  return <img className="w-full" src={imageUrl} alt={`Fred ${color}`} />;
+};
