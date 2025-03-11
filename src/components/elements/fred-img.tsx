f15ffae5ad09a7dc2bd578ab49ff85c5716909e4
Foursqweez <jeerasak.code@gmail.com>
2025-03-11 17:31:10 +0700

    Basic tween : gsap.to  with delay, repeat and yoyo

--- src/components/elements/fred-img.tsx
+++ src/components/elements/fred-img.tsx
@@ -2,46 +2,29 @@
 import { cn } from "@/lib/utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP);
 
 export default function FredImg() {
   const fredRef = useRef<null>(null);
-  const [start, setStart] = useState(false);
 
-  useGSAP(
-    () => {
-      setStart(true);
-      gsap.fromTo(
-        fredRef.current,
-        {
-          x: 700,
-          y: 400,
-          opacity: 0,
-        },
-        {
-          x: 400,
-          y: 200,
-          scale: 3,
-          opacity: 1,
-          duration: 3,
-        },
-      );
-
-      console.log("render in client");
-    },
-    { dependencies: [start], scope: fredRef },
-  );
+  useGSAP(() => {
+    gsap.to(".green", { duration: 2, x: 600, repeat: -1, yoyo: true });
+    gsap.to(".pink", { duration: 2, x: 600, delay: 2, repeat: -1, yoyo: true });
+  }, {});
 
   return (
     <div ref={fredRef} className="h-fit">
       <img
         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred.svg"
         alt=""
-        className={cn("fred hidden h-auto w-fit object-fill", {
-          block: start,
-        })}
+        className={cn("green h-auto w-fit object-fill")}
+      />
+      <img
+        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred-pink.svg"
+        alt=""
+        className={cn("pink h-auto w-fit object-fill")}
       />
     </div>
   );
