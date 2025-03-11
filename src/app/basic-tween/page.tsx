ee89870bf5e987ffca0fd489ede798731dfc70f9
Foursqweez <jeerasak.code@gmail.com>
2025-03-11 17:18:34 +0700

    Basic tween : gsap.fromTo() - Animate fred with basic tween using ref

--- src/app/basic-tween/page.tsx
+++ src/app/basic-tween/page.tsx
@@ -3,7 +3,7 @@ import FredImg from "@/components/elements/fred-img";
 export default function Page() {
   console.log("render in server");
   return (
-    <div className="bg-kitty-darker flex h-screen w-full">
+    <div className="bg-kitty-darker relative flex h-screen w-full overflow-hidden">
       <FredImg />
     </div>
   );
--- src/components/elements/fred-img.tsx
+++ src/components/elements/fred-img.tsx
@@ -1,27 +1,48 @@
 "use client";
+import { cn } from "@/lib/utils";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function FredImg() {
   const fredRef = useRef<null>(null);
+  const [start, setStart] = useState(false);
 
-  useGSAP(() => {
-    gsap.from(fredRef.current, {
-      x: 400,
-      y: 200,
-      scale: 3,
-      duration: 3,
-    });
+  useGSAP(
+    () => {
+      setStart(true);
+      gsap.fromTo(
+        fredRef.current,
+        {
+          x: 700,
+          y: 400,
+          opacity: 0,
+        },
+        {
+          x: 400,
+          y: 200,
+          scale: 3,
+          opacity: 1,
+          duration: 3,
+        },
+      );
 
-    console.log("render in client");
-  });
+      console.log("render in client");
+    },
+    { dependencies: [start], scope: fredRef },
+  );
+
   return (
-    <img
-      ref={fredRef}
-      src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred.svg"
-      alt=""
-      className="h-fit object-fill"
-    />
+    <div ref={fredRef} className="h-fit">
+      <img
+        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/32887/fred.svg"
+        alt=""
+        className={cn("fred hidden h-auto w-fit object-fill", {
+          block: start,
+        })}
+      />
+    </div>
   );
 }
