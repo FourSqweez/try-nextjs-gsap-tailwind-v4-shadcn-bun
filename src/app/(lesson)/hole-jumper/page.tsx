d8a3b936218e8b70ce85a6efbe579dcb21f7d0ac
Foursqweez <jeerasak.code@gmail.com>
2025-03-23 12:39:53 +0700

    Adding Squash and Stretch

--- src/app/(lesson)/hole-jumper/page.tsx
+++ src/app/(lesson)/hole-jumper/page.tsx
@@ -8,40 +8,58 @@ import { useRef } from "react";
 gsap.registerPlugin(GSDevTools);
 
 export default function Page() {
-  const tl = useRef<gsap.core.Timeline | null>(null);
   const herman = useRef<HTMLImageElement | null>(null);
   const hole = useRef<HTMLDivElement | null>(null);
+  const demo = useRef<HTMLDivElement | null>(null);
 
-  tl.current = gsap.timeline({
-    repeat: 0,
-  });
+  gsap.config({
+    trialWarn: false,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any);
 
-  useGSAP(() => {
-    if (!tl.current) return;
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        repeat: 0,
+      });
 
-    gsap.set(".demo", { autoAlpha: 1 });
+      tl.from(hole.current, { scale: 0, yoyo: true, repeat: 1 })
+        .fromTo(
+          herman.current,
+          { y: 160, scaleY: 2 },
+          { y: -175, scaleY: 1 },
+          0.2,
+        )
+        .to(herman.current, { y: 0, ease: "power1.in" }, ">")
+        .to(herman.current, {
+          scaleY: 0.8,
+          scaleX: 1.3,
+          transformOrigin: "50% 100%",
+          repeat: 1,
+          yoyo: true,
+          duration: 0.1,
+        });
 
-    // start animation here
+      const devTools = GSDevTools.create({
+        animation: tl,
+        loop: false,
+      });
 
-    tl.current
-      .from(hole.current, { scale: 0, yoyo: true, repeat: 1 })
-      .fromTo(herman.current, { y: 110 }, { y: -175 }, 0.2)
-      .to(herman.current, { y: 0, ease: "power1.in" }, ">");
-
-    const devTools = GSDevTools.create({
-      animation: tl.current,
-      loop: false,
-    });
-
-    return () => {
-      tl.current?.kill();
-      devTools.kill();
-    };
-  });
+      return () => {
+        tl.restart();
+        tl.kill();
+        devTools.kill();
+      };
+    },
+    { scope: demo, revertOnUpdate: true },
+  );
 
   return (
     <div className="m-0 flex h-screen w-full items-center justify-center bg-[20253f]">
-      <div className="demo relative size-[400px] border-[1px] border-solid border-gray-500 bg-[#d6deff]">
+      <div
+        ref={demo}
+        className="demo relative size-[400px] border-[1px] border-solid border-gray-500 bg-[#d6deff]"
+      >
         <div
           ref={hole}
           className="hole absolute top-[290px] left-1/2 h-[20px] w-[150px] -translate-x-1/2 rounded-[50%] bg-black"
